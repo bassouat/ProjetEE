0932bd184eda12ea47475d8f452d0c8409ac9dbf
Basseydou Ouattara <bassouat8@gmail.com>
2021-11-21 06:23:35 +0100

    simplification de la condition if

--- src/main/java/com/example/projetJEE/UserValidationService.java
+++ src/main/java/com/example/projetJEE/UserValidationService.java
@@ -2,9 +2,10 @@ package com.example.projetJEE;
 
 public class UserValidationService {
     public boolean isUserValid(String user, String password) {
-        if (user.equals("ouattara") && password.equals("reussite"))
-            return true;
-        return false;
+        return user.equals("la magie") && password.equals("reussite1890");
 
     }
+
+
+
 }
